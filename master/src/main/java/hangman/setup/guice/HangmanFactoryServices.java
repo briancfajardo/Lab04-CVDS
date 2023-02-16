279f01429ed71d4bf205a9f1013670a88fa9f163
unknown <brian.fajardo@labinfo.is.escuelaing.edu.co>
2023-02-16 15:59:23 -0500

    Primer commit

---
+++ master/src/main/java/hangman/setup/guice/HangmanFactoryServices.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hangman.setup.guice;
+
+/**
+ *
+ * @author 2106913
+ */
+
+import hangman.model.French;
+import hangman.model.Language;
+import hangman.model.dictionary.HangmanDictionary;
+import hangman.model.dictionary.FrenchDictionaryDataSource;
+import hangman.view.HangmanNoviolentoPanel;
+import hangman.view.HangmanPanel;
+
+public class HangmanFactoryServices extends com.google.inject.AbstractModule {
+
+    @Override
+    protected void configure() {
+        /* Guice dependency injection */
+        // bind(Interface.class).to(Concrete.class);
+    }
+
+}
